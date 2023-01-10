6662d14aae25cdd5aa2300c5ae5d0e167208e98d
Dagan3D <butmanovdanil@gmail.com>
2023-01-10 15:12:22 +0500

    Lab 2_4_1 complete

---
+++ Lab_2/Lab_2_4_1.py
@@ -0,0 +1,8 @@
+import os
+import random
+
+os.mkdir("example")
+print(123)
+for i in range(100):
+    with open("./example/" + str(i)+ ".txt", "wb") as file:
+        file.write(random.randbytes(random.randint(1024, 102400)))
