016b4eed677be8245635f03879efc5f4ecc946f6
Dagan3D <butmanovdanil@gmail.com>
2023-01-10 18:06:41 +0500

    Lab 2_6_1 complete

---
+++ Lab_2/Lab_2_6_1.py
@@ -0,0 +1,14 @@
+
+alphavit = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю']
+
+alphavit = {key: 0 for key in alphavit}
+
+with open("article_rus.txt", "r", encoding="utf8") as file:
+    str = file.read()
+
+for chr in str:
+    if chr.lower() in alphavit.keys():
+        alphavit[chr.lower()] += 1
+
+print(alphavit)
